f7d8cdea1c1ec7c4f6245718dac657f20478e296
Marsel <marselbisengaliev1@gmail.com>
2022-09-10 16:38:31 +0500

    Trying to write the right path to qoutes.json

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
         btn = document.getElementById('btn');
         let lastIndex = 0;
 
-    fetch('/quotes.json')
+    fetch('./quotes.json')
         .then(res => {
             return res.json()
         })
